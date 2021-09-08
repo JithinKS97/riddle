089736dae573744121919682d88356248d767bcc
Jithin KS <jithinks@Jithins-MBP.domain.name>
2021-09-08 19:06:58 +0530

    Resized canvas

--- components/Drawingboard.js
+++ components/Drawingboard.js
@@ -2,23 +2,27 @@ import { fabric } from "fabric";
 import { forwardRef, useEffect, useImperativeHandle } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-let canvas,
-  canvasConfig = {
-    isDrawingMode: true,
-    width: 640,
-    height: 360,
-    backgroundColor: "rgba(0,0,0,0)",
-  };
+let canvas;
 
 const Drawingboard = forwardRef((props, ref) => {
   const { onAddPath } = props;
 
   useEffect(() => {
-    canvas = new fabric.Canvas("c", canvasConfig);
+    canvas = createCanvas();
     canvas.freeDrawingBrush.color = "black";
     canvas.freeDrawingBrush.width = 3;
     registerEvents();
   }, []);
+
+  const createCanvas = () => {
+    const canvasConfig = {
+      isDrawingMode: true,
+      width: window.innerWidth,
+      height: window.innerHeight,
+      backgroundColor: "rgba(0,0,0,0)",
+    };
+    return new fabric.Canvas("c", canvasConfig);
+  };
 
   const registerEvents = () => {
     canvas.on("path:created", (res) => {
