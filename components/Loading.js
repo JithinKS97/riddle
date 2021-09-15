99bc76d7a6bfd7fcb05cd6514a3b1d2fd46d6145
Jithin KS <jithinks@Jithins-MBP.domain.name>
2021-09-15 15:57:47 +0530

    Loading

--- components/Loading.js
+++ components/Loading.js
@@ -1,16 +1,21 @@
-import { Container, Spinner, Center } from "@chakra-ui/react";
+import { Container, Spinner, Center, Box, VStack } from "@chakra-ui/react";
 
 const Loading = () => {
   return (
     <Container>
       <Center height="100vh">
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="#1A365D"
-          size="xl"
-        />
+        <VStack>
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="#1A365D"
+            size="xl"
+          />
+          <Box position="relative" left="5px">
+            Loading...
+          </Box>
+        </VStack>
       </Center>
     </Container>
   );
