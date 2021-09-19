a12a868d277fdd268526c2538c26530611c17aac
Jithin KS <jithinks@Jithins-MBP.domain.name>
2021-09-19 15:05:19 +0530

    Clean up

--- components/drawingboard/mode.js
+++ components/drawingboard/mode.js
@@ -1,58 +1,60 @@
 import { drawModes } from "../../constant/menu";
-import { Pencil, Pan, Select, None, Rectangle } from "../../constant/mode";
+import { Pencil, Pan, Select, None } from "../../constant/mode";
 
 export const onModeChange = (canvas, selectedMode) => () => {
   if (!canvas) {
     return;
   }
 
-  const disableSelect = () => {
-    canvas.set({
-      selection: false,
-    });
-    canvas.getObjects().forEach((object) => {
-      object.set({ selectable: false });
-    });
-    canvas.discardActiveObject();
-    canvas.renderAll();
-  };
-
-  const enableSelect = () => {
-    canvas.set({
-      selection: true,
-    });
-    canvas.getObjects().forEach((object) => {
-      object.set({ selectable: true });
-    });
-  };
-
   const isShapeDrawingMode =
     drawModes.includes(selectedMode) && selectedMode !== Pencil;
 
-  if (isShapeDrawingMode) {
-    disableSelect();
-    canvas.defaultCursor = "crosshair";
-    canvas.isDrawingMode = false;
-    return;
-  } else if (selectedMode === Pencil) {
+  if (selectedMode === Select) {
+    enableSelect(canvas);
+  } else {
+    disableSelect(canvas);
+  }
+
+  if (selectedMode === Pencil) {
     canvas.isDrawingMode = true;
-    disableSelect();
+  } else {
+    canvas.isDrawingMode = false;
+  }
+
+  if (isShapeDrawingMode) {
+    canvas.defaultCursor = "crosshair";
+    canvas.hoverCursor = "crosshair";
     return;
   } else if (selectedMode === Select) {
-    enableSelect();
     canvas.defaultCursor = "auto";
     canvas.hoverCursor = "move";
-    canvas.isDrawingMode = false;
     return;
   } else if (selectedMode === Pan) {
-    disableSelect();
-    canvas.isDrawingMode = false;
     canvas.defaultCursor = "grab";
     canvas.hoverCursor = "grab";
     return;
   } else if (selectedMode === None) {
-    canvas.isDrawingMode = false;
     canvas.defaultCursor = "auto";
     canvas.hoverCursor = "auto";
   }
+};
+
+const disableSelect = (canvas) => {
+  canvas.set({
+    selection: false,
+  });
+  canvas.getObjects().forEach((object) => {
+    object.set({ selectable: false });
+  });
+  canvas.discardActiveObject();
+  canvas.renderAll();
+};
+
+const enableSelect = (canvas) => {
+  canvas.set({
+    selection: true,
+  });
+  canvas.getObjects().forEach((object) => {
+    object.set({ selectable: true });
+  });
 };
